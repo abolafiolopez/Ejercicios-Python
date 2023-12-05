23637b2e1123672b42051056f1833be8ea0c8a23
abolafiolopez <abolafiolopez@gmail.com>
2023-12-05 19:28:54 +0100

    Web scraping de libros inferiores a £20

--- WebScraping/Src/topscrape_precios.py
+++ WebScraping/Src/topscrape_precios.py
@@ -1,20 +1,53 @@
 """
-Web scraping de la página unplash para sacar las imágenes
+Web scraping de la página toscrape.com
+para obtener los títulos de los libros con 4 o 5 estrellas
+más baratos de 20 Libras
 """
 
+# Importaciones
 import bs4
 import requests
+import re
 
-resultado = requests.get('https://escueladirecta.com/courses')
+# Variables
+url_base = 'http://books.toscrape.com/catalogue/page-{}.html'
+lista_titulos = []
+lista_precios = []
+patron_precio = r'\d{2}.\d{2}'
 
-sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
+# Bucle para iterar páginas
+for i in range(1, 51):
 
-imagenes = sopa.select('.course-box-image')[0]['src']
+    # Crear sopa de cada página
+    url_pagina = url_base.format(i)
+    resultado = requests.get(url_pagina)
+    sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
 
-imagen_curso_1 = requests.get(imagenes)
-'''print(imagen_curso_1)
-print(imagen_curso_1.content)'''
+    # Seleccionar datos de los libros
+    libros = sopa.select('.product_pod')
 
-imagen = open('imagen_curso1.jpg', 'wb')
-imagen.write(imagen_curso_1.content)
-imagen.close()
+    # Bucle para iterar libros
+    for libro in libros:
+
+        # Chequear 4 o 5 estrellas
+        if len(libro.select('.star-rating.Four')) != 0 or len(libro.select('.star-rating.Five')) != 0:
+
+            # Chequear precio
+            precios = libro.select('.price_color')[0]
+
+            # Bucle para encontrar el precio y guardarlo en la lista
+            for p in precios:
+                buscar_precio = re.search(patron_precio, p)
+                precio_libro = buscar_precio.group()
+
+                # Guardar precio si es menor a £20
+                if float(precio_libro) < 20:
+                    lista_precios.append(precio_libro)
+
+                    # Guardar título en la lista
+                    titulo_libro = libro.select('a')[1]['title']
+                    lista_titulos.append(titulo_libro)
+
+# Bucle para imprimir cada título de la lista y su precio
+for titulo, precio in zip(lista_titulos, lista_precios):
+    print(f'{titulo}: £{precio}')
